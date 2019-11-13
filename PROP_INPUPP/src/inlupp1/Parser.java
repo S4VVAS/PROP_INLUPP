874bc2902da9e47c29c4b29f86b740055c2caf6b
Savvas <savvasswe@gmail.com>
2019-11-13 13:50:44 +0100

    >Parser done

--- PROP_INPUPP/src/inlupp1/Parser.java
+++ PROP_INPUPP/src/inlupp1/Parser.java
@@ -9,7 +9,7 @@ public class Parser implements IParser {
 	public void open(String fileName) throws IOException, TokenizerException {
 		token = new Tokenizer();
 		token.open(fileName);
-		
+
 	}
 
 	@Override
@@ -19,32 +19,32 @@ public class Parser implements IParser {
 
 	@Override
 	public void close() throws IOException {
-		if(token != null)
+		if (token != null)
 			token.close();
 	}
-	
+
 	private String tabs(int amt) {
 		String tabs = "";
-		for(int i = 0; i < amt; i++) {
+		for (int i = 0; i < amt; i++) {
 			tabs = tabs + "\t";
 		}
 		return tabs;
 	}
-	
+
 	private void build(StringBuilder builder, int tabs) {
 		builder.append(tabs(tabs) + token.current().token() + " " + token.current().value() + "\n");
 	}
 	
-	//______________________________________________________________________________________________
-	//______________________________________________________________________________________________
-	//______________________________________________________________________________________________
-	
+	// ______________________________________________________________________________________________
+	// ______________________________________________________________________________________________
+	// ______________________________________________________________________________________________
+
 	class AssignmentNode implements INode {
 		String id;
 		char assign;
 		char semCol;
 		INode exp;
-		
+
 		@Override
 		public Object evaluate(Object[] args) throws Exception {
 			// TODO Auto-generated method stub
@@ -52,33 +52,34 @@ public class Parser implements IParser {
 		}
 
 		@Override
-		public void buildString(StringBuilder builder, int tabs) throws IOException, TokenizerException, ParserException {
+		public void buildString(StringBuilder builder, int tabs)
+				throws IOException, TokenizerException, ParserException {
 			builder.append("AssignmentNode\n");
+			
 			token.moveNext();
-			if(token.current().token() == Token.IDENT) {
+			if (token.current().token() == Token.IDENT) {
 				build(builder, tabs);
 				token.moveNext();
-				if(token.current().token() == Token.ASSIGN_OP) {
+				if (token.current().token() == Token.ASSIGN_OP) {
 					build(builder, tabs);
+					token.moveNext();
 					exp = new ExpressionNode();
 					exp.buildString(builder, tabs + 1);
 					
-					token.moveNext();
-					if(token.current().token() == Token.SEMICOLON)
+					if (token.current().token() == Token.SEMICOLON)
 						build(builder, tabs);
 					else
 						throw new ParserException("Semicolon missing from assignment: " + token.current().value());
-				
-				}
-				else
+
+				} else
 					throw new ParserException("Assignment of id missing: " + token.current().value());
 			} else
-					throw new ParserException("Wrong id: " + token.current().value());	
+				throw new ParserException("Wrong id: " + token.current().value());
 		}
 
 	}
-	//______________________________________________________________________________________________
-	
+	// ______________________________________________________________________________________________
+
 	class ExpressionNode implements INode {
 		INode term, exp;
 		char arOp;
@@ -90,24 +91,25 @@ public class Parser implements IParser {
 		}
 
 		@Override
-		public void buildString(StringBuilder builder, int tabs) throws IOException, TokenizerException, ParserException {
+		public void buildString(StringBuilder builder, int tabs)
+				throws IOException, TokenizerException, ParserException {
 			builder.append(tabs(tabs - 1) + "ExpressionNode\n");
-			
+
 			term = new TermNode();
 			term.buildString(builder, tabs + 1);
+
 			
-			token.moveNext();
-			if(token.current().token() == Token.ADD_OP || token.current().token() == Token.SUB_OP)
+			if (token.current().token() == Token.ADD_OP || token.current().token() == Token.SUB_OP) {
 				build(builder, tabs);
-			else
-				throw new ParserException("Arithmetic Operator missing(+/-): " + token.current().value());
-			
-			exp = new ExpressionNode();
-			exp.buildString(builder, tabs + 1);
+				token.moveNext();
+				
+				exp = new ExpressionNode();
+				exp.buildString(builder, tabs + 1);
+			}
 		}
 	}
-	//______________________________________________________________________________________________
-	
+	// ______________________________________________________________________________________________
+
 	class TermNode implements INode {
 		INode fact, term;
 		char arOp;
@@ -119,25 +121,26 @@ public class Parser implements IParser {
 		}
 
 		@Override
-		public void buildString(StringBuilder builder, int tabs) throws IOException, TokenizerException, ParserException {
+		public void buildString(StringBuilder builder, int tabs)
+				throws IOException, TokenizerException, ParserException {
 			builder.append(tabs(tabs - 1) + "TermNode\n");
-			
+
 			fact = new FactorNode();
 			fact.buildString(builder, tabs + 1);
-			
-			token.moveNext();
-			if(token.current().token() == Token.MULT_OP || token.current().token() == Token.DIV_OP)
+
+		
+			if (token.current().token() == Token.MULT_OP || token.current().token() == Token.DIV_OP) {
 				build(builder, tabs);
-			else
-				throw new ParserException("Arithmetic Operator missing(*//): " + token.current().value());
-			
-			term = new TermNode();
-			term.buildString(builder, tabs + 1);
+				token.moveNext();
+				
+				term = new TermNode();
+				term.buildString(builder, tabs + 1);
+			}
 		}
 
 	}
-	//______________________________________________________________________________________________
-	
+	// ______________________________________________________________________________________________
+
 	class FactorNode implements INode {
 		int num;
 		char lP, rP;
@@ -150,70 +153,33 @@ public class Parser implements IParser {
 		}
 
 		@Override
-		public void buildString(StringBuilder builder, int tabs) throws IOException, TokenizerException, ParserException {
+		public void buildString(StringBuilder builder, int tabs)
+				throws IOException, TokenizerException, ParserException {
 			builder.append(tabs(tabs - 1) + "FactorNode\n");
+
 			
-			token.moveNext();
-			if(token.current().token() == Token.INT_LIT || token.current().token() == Token.IDENT) {
+			if (token.current().token() == Token.INT_LIT) {
 				build(builder, tabs);
-			}
-			else if(token.current().token() == Token.LEFT_PAREN) {
+				token.moveNext();
+			} else if (token.current().token() == Token.LEFT_PAREN) {
 				build(builder, tabs);
-				
+				token.moveNext();
+
 				exp = new ExpressionNode();
 				exp.buildString(builder, tabs + 1);
-				
-				token.moveNext();
-				if(token.current().token() == Token.LEFT_PAREN) {
+
+			
+				if (token.current().token() == Token.RIGHT_PAREN) {
 					build(builder, tabs);
-				}
-				else
+					token.moveNext();
+				} else
 					throw new ParserException("Closing parentheses missing: " + token.current().value());
 
-			}
-			else
+			} else
 				throw new ParserException("False factor: " + token.current().value());
-			
-			
+
 		}
 
 	}
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
